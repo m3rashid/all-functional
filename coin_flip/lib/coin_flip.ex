a06487bf6bf6ceaa671af2dee8ce6b0b33a8cd4e
m3rashid <m3rashid.hussain@gmail.com>
2023-11-22 19:17:52 +0530

    init: I am BATMAN

---
+++ coin_flip/lib/coin_flip.ex
@@ -0,0 +1,18 @@
+defmodule CoinFlip do
+  @moduledoc """
+  Documentation for `CoinFlip`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CoinFlip.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
